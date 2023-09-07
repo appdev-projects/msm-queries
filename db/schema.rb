1eb7c923a5bba1569ff8bfd76e9e43b98ff17fba
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-09-07 19:36:34 +0000

    generated character tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_07_193528) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_07_193610) do
   create_table "actors", force: :cascade do |t|
     t.string "name"
     t.date "dob"
     t.text "bio"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "actor_id"
+    t.integer "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
