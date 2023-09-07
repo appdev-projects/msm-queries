7b545ab0373387f214ada7997e3a924ac80796d7
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-09-07 19:19:20 +0000

    added director movie and actor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "misc", :action => "homepage" })
+  #get("/", { :controller => "misc", :action => "homepage" })
+
+  root to: "misc#homepage"
+
+  get "/directors", to: "directors#index"
+
+  get "/actors", to: "actors#index"
+
+  get "/movies", to: "movies#index"
+
+
+
 end
